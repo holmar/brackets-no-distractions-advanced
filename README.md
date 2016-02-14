52c0470a3f961f99b5d5961b5ea4fd6570332196
Martin Holler <servus@martinholler.com>
2016-02-14 20:50:50 +0100

    Added extension files & README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# brackets-no-distractions-advanced
-Brackets extension that displays the sidebar/toolbar when pointing at the left/right side of the editor.
+# Brackets No Distraction Advanced
+Extends the "No Distractions" view to display the sidebar when pointing at the left side of the editor and the toolbar when pointing at the right side.
+
+![overlay](https://cloud.githubusercontent.com/assets/7590968/13035716/6c5935da-d356-11e5-9908-0c589daa5051.gif)
+
+## Installation
+Open the [Extension Manager](https://github.com/adobe/brackets/wiki/Brackets-Extensions) (File → Extension Manager), search for "No Distractions Advanced" and hit install.
+
+## Usage
+Enable the "No Distractions" view (View → No Distractions) and point to the left/right edge of the editor.
+
+## Options
+The extension provides two display modes: **overlay** (default) and **push**. In **overlay** mode, the sidebar/toolbar will be on top of the content area (see example image above), while in **push** mode, the content area will be pushed off-canvas:
+
+![push](https://cloud.githubusercontent.com/assets/7590968/13035717/6c85b07e-d356-11e5-996f-24382fa6533f.gif)
+
+To set the mode, open the Preferences file (Brackets → Preferences) and add the following setting:
+
+``"no-distractions-advanced.mode": "push"``
---
+++ main.js
@@ -0,0 +1,131 @@
+/**
+ * No Distractions Advanded Extension
+ * Extends the "No Distractions" view to display the sidebar when pointing at
+ * the left side of the editor and the toolbar when pointing at the right side.
+ *
+ * @author Martin Holler
+ * @version 1.0.0
+ * @license MIT
+ */
+
+define(function (require, exports, module) {
+    'use strict';
+
+    var LEFT_OFFSET = 20;
+    var MODES = ['overlay', 'push'];
+
+    // imports
+    var ExtensionUtils = brackets.getModule('utils/ExtensionUtils');
+    var PreferencesManager = brackets.getModule('preferences/PreferencesManager');
+
+    // elements
+    var $body = $(document.body);
+    var $sidebar = $('#sidebar');
+    var $toolbar = $('#main-toolbar');
+    var $editor = $('#editor-holder');
+    var $mainView = $('.main-view');
+
+    // preferences
+    var extensionPrefs = PreferencesManager.getExtensionPrefs('no-distractions-advanced');
+    var mode;
+
+    var enable = function () {
+        var state = 0; // 0 = closed, 1 = sidebar open, 2 = toolbar open
+        var lastState = state;
+        var sidebarWidth = $sidebar.width();
+        var toolbarWidth = $toolbar.width();
+
+        // add event listeners
+        $editor.off('mousemove.NDA').on('mousemove.NDA', function (e) {
+
+            // bail if the mouse is pressed
+            if (e.which) {
+                return;
+            }
+
+            if (e.clientX < LEFT_OFFSET || (state === 1 && e.clientX < sidebarWidth)) {
+                state = 1;
+            } else if (e.clientX > $editor.width() - toolbarWidth) {
+                state = 2;
+            } else {
+                state = 0;
+            }
+
+            // emit events on state change
+            if (state !== lastState) {
+                $editor.trigger('NDAStateChange', state);
+            }
+
+            lastState = state;
+        });
+
+        $editor.off('NDAStateChange').on('NDAStateChange', function (e, state) {
+            var pushValue = 0;
+
+            $body.removeClass('nda--left nda--right');
+
+            switch (state) {
+            case 1:
+                pushValue = sidebarWidth;
+                $body.addClass('nda--left');
+                break;
+            case 2:
+                pushValue = -toolbarWidth;
+                $body.addClass('nda--right');
+                break;
+            }
+
+            // push mode
+            if (mode === 'push') {
+                $mainView.css('transform', 'translateX(' + pushValue + 'px)');
+            }
+        });
+
+        $body.addClass('nda');
+    };
+
+    var disable = function () {
+        $body.removeClass('nda nda--left nda--right');
+        $mainView.removeAttr('style');
+        $editor.off('mousemove.NDA NDAStateChange');
+    };
+
+    $sidebar.addClass('nda-target nda-target--left');
+    $toolbar.addClass('nda-target nda-target--right');
+
+    // load the stylesheet
+    ExtensionUtils.loadStyleSheet(module, 'styles.css');
+
+    // watch for preferences changes (this is also called on launch)
+    PreferencesManager.on('change', 'noDistractions', function () {
+
+        // enable the extension when the noDistractions is active
+        if (PreferencesManager.get('noDistractions')) {
+            enable();
+
+        // otherwise, disable it
+        } else {
+            disable();
+        }
+    });
+
+    extensionPrefs.definePreference('mode', 'string', MODES[0]).on('change', function () {
+        mode = extensionPrefs.get('mode');
+
+        // return to default if value is invalid
+        if (MODES.indexOf(mode) === -1) {
+            mode = MODES[0];
+        }
+
+        // remove style attribute if not in push mode
+        if (mode !== 'push') {
+            $mainView.removeAttr('style');
+        }
+
+        $body.attr('data-nda-mode', mode);
+    });
+
+    // exports
+    exports.enable = enable;
+    exports.disable = disable;
+});
